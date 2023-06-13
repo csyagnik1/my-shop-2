0807c9f75fcad76089e71648a1f3b4b1a8348ccf
csyagnik1 <csyagnik@gmail.com>
2023-06-13 21:23:27 +0530

    Update productSaga.js

--- src/redux/productSaga.js
+++ src/redux/productSaga.js
@@ -3,10 +3,10 @@ import {  PRODUCT_LIST, SEARCH_PRODUCT, SET_PRODUCT_LIST } from './constant';
 
 
 function* getProducts(){
-     let data = yield fetch("https://api.jsonbin.io/v3/b/647ef2e49d312622a36b3219");
+     let data = yield fetch(`https://my-json-db.onrender.com/product`);
     
     data = yield data.json();
-    data = data.record.product;
+    // data = data.record.product;
     console.log("Data from bin",data);
 
     
@@ -14,7 +14,7 @@ function* getProducts(){
 }
 
 function* searchProducts(data){
-    let result = yield fetch("https://api.jsonbin.io/v3/b/647ef2e49d312622a36b3219");
+    let result = yield fetch(`https://my-json-db.onrender.com/product?q=${data.query}`);
     result = yield result.json();
     console.log("action is called", data)
     yield put({type: SET_PRODUCT_LIST, data:result})
